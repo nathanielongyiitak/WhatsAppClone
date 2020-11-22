fd949420252b718be98d1b365da03674d6f5531d
nathanielongyiitak <hinathanielong@gmail.com>
2020-11-23 00:05:13 +0800

    Implemented navigation

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,12 +1,13 @@
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { MaterialCommunityIcons, Octicons } from '@expo/vector-icons';
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
-
+import { ColorSchemeName, View } from 'react-native';
+import Colors from '../constants/Colors';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
+import MainTabNavigator from './MainTabNavigator';
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -26,8 +27,28 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
+    <Stack.Navigator screenOptions={{
+      headerStyle: {
+        backgroundColor: Colors.light.tint,
+        shadowOpacity: 0,
+        elevation: 0
+      },
+      headerTintColor: Colors.light.background,
+      headerTitleStyle: { fontWeight: 'bold' },
+    }}>
+      <Stack.Screen
+        name="Root"
+        component={MainTabNavigator}
+        options={{
+          title: 'WhatsApp',
+          headerRight: () => (
+            <View style={{ flexDirection: 'row', width: 60, justifyContent: 'space-between', marginRight: 10 }}>
+              <Octicons name='search' size={22} color={Colors.light.background} />
+              <MaterialCommunityIcons name='dots-vertical' size={22} color={Colors.light.background} />
+            </View>
+          )
+        }}
+      />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
