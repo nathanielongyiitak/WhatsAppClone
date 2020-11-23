e4e05f84ef49b5c0b88b7ed062562bdbd0567bce
nathanielongyiitak <hinathanielong@gmail.com>
2020-11-23 21:41:09 +0800

    Implement api and user model

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
@@ -12,9 +12,55 @@ Amplify.configure(config);
 
 import { withAuthenticator } from 'aws-amplify-react-native';
 
+import { Auth, API, graphqlOperation } from 'aws-amplify';
+import { getUser } from './graphql/queries';
+import { createUser } from './graphql/mutations';
+
+const randomImages = [
+  'https://images.unsplash.com/photo-1605915492188-59ca97692e38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  'https://images.unsplash.com/photo-1605958589938-c95297817857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80',
+  'https://images.unsplash.com/photo-1605973246991-c7e2a18882ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  'https://images.unsplash.com/photo-1606025454157-e822085acd4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+  'https://images.unsplash.com/photo-1606037163262-243e5a63c696?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1041&q=80',
+];
+
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * randomImages.length)];
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub }),
+        );
+
+        if (userData.data.getUser) {
+          console.log('User already exists.');
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: 'Hey, I am using WhatsApp!',
+        };
+
+        await API.graphql(graphqlOperation(createUser, { input: newUser }));
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
